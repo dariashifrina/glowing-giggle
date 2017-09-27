aba0fd60ba7b55f26f5000f4227014cc6fd94d96
dariashifrina <dshifrina@stuy.edu>
2017-09-26 20:53:51 -0400

    random occupation link works

--- app.py
+++ app.py
@@ -16,27 +16,34 @@ cool_list = cool_list[:len(cool_list)-1]
 
 while (i<len(cool_list)):
      thisLine = cool_list[i] #Let's grab the current line
-     comma = thisLine.rfind(",") #Let's also find the right comma - the one that seperates the number and value
-     cool_dic[thisLine[:comma].strip('"')] = thisLine[comma + 1:].strip('\n').strip("\r") #Add the stuff before the comma (occupation) to the dictionary as a key, then add the stuff after the comma as a value!
+     last_comma = thisLine.rfind(",") #Let's also find the right comma - the one that seperates the number and value
+     prelast_comma = thisLine[:last_comma].rfind(",")
+     occupation =  thisLine[:prelast_comma].strip('"')
+     percentage = thisLine[prelast_comma + 1: last_comma].strip('\n').strip("\r")
+     link = thisLine[last_comma + 1:].strip('\n').strip("\r")
+     cool_dic.setdefault(occupation, []).append(percentage)
+     cool_dic.setdefault(occupation, []).append(link)
+     #cool_dic[thisLine[:prelast_comma].strip('"')] = thisLine[comma + 1:].strip('\n').strip("\r") #Add the stuff before the comma (occupation) to the dictionary as a key, then add the stuff after the comma as a value!
      i += 1 #Wouldn't want an infinite loop now would we
 
-#print(cool_dic)
-
-#explanation: we used cumulative probability, since random does not account for weighted probabilities as all of the values in its range have an equal probability of being picked. first, we generated a random float that stood for a random percentage(0-100%). then we summed up the dictionary probabilities in the cumulative_probability. the moment the cumulative probability exceeded the random percentage, we returned that value. this method is efficient because dictionary keys are picked in random order so there's no order ruining the randonmness(a parallel would be a bag with different amounts of colorful balls and randomly taking out one!)
 def random_occupation():
     c = 0 #counter for while loop
     random_float = random.uniform(0.0, 99.8) #float within range
     #print random_float
     cumulative_probability = 0.0
     while (c < len(cool_dic)):
-        cumulative_probability += float(cool_dic.values()[c])
+        cumulative_probability += float(cool_dic.values()[c][0])
         if (cumulative_probability>random_float):
             return cool_dic.keys()[c]
         c += 1
-
+                                                                           
+def occupation_link(occupation):
+    return cool_dic[occupation][1]
+                                                                           
 @app.route("/occupations")
 def occupations():
-	return render_template( 'occ_page.html', my_dict = cool_dic, my_occ = random_occupation())
+        occupation = random_occupation()
+	return render_template( 'occ_page.html', my_dict = cool_dic, my_occ = occupation, my_link = occupation_link(occupation))
 
 if __name__ == "__main__":
 	app.debug = True
