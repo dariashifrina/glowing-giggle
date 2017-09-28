3f605222fa128f405d7a03b9bf77651178a6cbb0
Irene Lam <irenelam@Irenes-MacBook-Pro.local>
2017-09-28 13:14:27 -0400

    Refactored code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect
+from util import occupationHelper
 import random
 app = Flask(__name__)
 
@@ -6,49 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return redirect("/occupations")
-
-#Creation of dictionary
-cool_file = open("data/occupations.csv", "r") #Open up our csv file so we can get to working on it
-cool_list = cool_file.readlines() #Read the file in a way that each line is a list item
-cool_file.close()
-#print coolList
-cool_dic = {} #Close up the file
-i = 1 #this is going to be the index of our list!!
-cool_list = cool_list[:len(cool_list)-1]
-
-while (i<len(cool_list)):
-     thisLine = cool_list[i] #Let's grab the current line
-     last_comma = thisLine.rfind(",") #Let's also find the right comma - the one that seperates the number and value
-     prelast_comma = thisLine[:last_comma].rfind(",")
-     occupation =  thisLine[:prelast_comma].strip('"')
-     percentage = thisLine[prelast_comma + 1: last_comma].strip('\n').strip("\r")
-     link = thisLine[last_comma + 1:].strip('\n').strip("\r")
-     cool_dic.setdefault(occupation, []).append(percentage)
-     cool_dic.setdefault(occupation, []).append(link)
-     #cool_dic[thisLine[:prelast_comma].strip('"')] = thisLine[comma + 1:].strip('\n').strip("\r") #Add the stuff before the comma (occupation) to the dictionary as a key, then add the stuff after the comma as a value!
-     i += 1 #Wouldn't want an infinite loop now would we
-
-#Returns a random occupation
-def random_occupation():
-    c = 0 #counter for while loop
-    random_float = random.uniform(0.0, 99.8) #float within range
-    #print random_float
-    cumulative_probability = 0.0
-    while (c < len(cool_dic)):
-        cumulative_probability += float(cool_dic.values()[c][0])
-        if (cumulative_probability>random_float):
-            return cool_dic.keys()[c]
-        c += 1
-
-#Returns the helpful link to the corresponding occupation	
-def occupation_link(occupation):
-    return cool_dic[occupation][1]
                                                                            
 @app.route("/occupations")
 def occupations():
-        occupation = random_occupation()
+        occupation = occupationHelper.random_occupation()
 	#Passes webpage from template, dictionary of Job Class, Percentage, and Link, and link to corresponding random profession
-	return render_template( 'occ_page.html', my_dict = cool_dic, my_occ = occupation, my_link = occupation_link(occupation))
+	return render_template( 'occ_page.html', my_dict = occupationHelper.get_dic(), my_occ = occupation, my_link = occupationHelper.occupation_link(occupation))
 
 if __name__ == "__main__":
 	app.debug = True
--- util/occupationHelper.py
+++ util/occupationHelper.py
@@ -1,16 +1,8 @@
+from flask import Flask, render_template, redirect
 import random
-from collections import defaultdict
-from util import occupationHelper
 
-
-'''
-Team BigOwl: Adam Abbas, Daria Shifrina
-SoftDev pd7
-HW#03: StI/O: Divine your Destiny!
-2017-09-15
-'''
-
-cool_file = open("occupations.csv", "r") #Open up our csv file so we can get to working on it
+#Creation of dictionary
+cool_file = open("data/occupations.csv", "r") #Open up our csv file so we can get to working on it
 cool_list = cool_file.readlines() #Read the file in a way that each line is a list item
 cool_file.close()
 #print coolList
@@ -20,20 +12,31 @@ cool_list = cool_list[:len(cool_list)-1]
 
 while (i<len(cool_list)):
      thisLine = cool_list[i] #Let's grab the current line
-     comma = thisLine.rfind(",") #Let's also find the right comma - the one that seperates the number and value
-     cool_dic[thisLine[:comma].strip('"')] = thisLine[comma + 1:].strip('\n').strip("\r") #Add the stuff before the comma (occupation) to the dictionary as a key, then add the stuff after the comma as a value!
+     last_comma = thisLine.rfind(",") #Let's also find the right comma - the one that seperates the number and value
+     prelast_comma = thisLine[:last_comma].rfind(",")
+     occupation =  thisLine[:prelast_comma].strip('"')
+     percentage = thisLine[prelast_comma + 1: last_comma].strip('\n').strip("\r")
+     link = thisLine[last_comma + 1:].strip('\n').strip("\r")
+     cool_dic.setdefault(occupation, []).append(percentage)
+     cool_dic.setdefault(occupation, []).append(link)
+     #cool_dic[thisLine[:prelast_comma].strip('"')] = thisLine[comma + 1:].strip('\n').strip("\r") #Add the stuff before the comma (occupation) to the dictionary as a key, then add the stuff after the comma as a value!
      i += 1 #Wouldn't want an infinite loop now would we
 
-#print(cool_dic)
-
-#explanation: we used cumulative probability, since random does not account for weighted probabilities as all of the values in its range have an equal probability of being picked. first, we generated a random float that stood for a random percentage(0-100%). then we summed up the dictionary probabilities in the cumulative_probability. the moment the cumulative probability exceeded the random percentage, we returned that value. this method is efficient because dictionary keys are picked in random order so there's no order ruining the randonmness(a parallel would be a bag with different amounts of colorful balls and randomly taking out one!) 
+#Returns a random occupation
 def random_occupation():
     c = 0 #counter for while loop
     random_float = random.uniform(0.0, 99.8) #float within range
     #print random_float
     cumulative_probability = 0.0
     while (c < len(cool_dic)):
-        cumulative_probability += float(cool_dic.values()[c])
+        cumulative_probability += float(cool_dic.values()[c][0])
         if (cumulative_probability>random_float):
             return cool_dic.keys()[c]
-c += 1
+        c += 1
+
+#Returns the helpful link to the corresponding occupation	
+def occupation_link(occupation):
+    return cool_dic[occupation][1]
+
+def get_dic():
+    return cool_dic
